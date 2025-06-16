e9cd0946aa9712d97f7bffe72e7ee4384b4eed27
Javiera Rahmadhany <hellorahmadhany@gmail.com>
2025-06-16 08:24:04 +0800

    feat: add dependency injection for barang and kategori modul

--- main.go
+++ main.go
@@ -1,7 +1,37 @@
 package main
 
-import "fmt"
+import (
+	"log"
+
+	"project-app-inventaris-cli-rahmadhany/cmd"
+	db "project-app-inventaris-cli-rahmadhany/database"
+	"project-app-inventaris-cli-rahmadhany/handler"
+	"project-app-inventaris-cli-rahmadhany/repository"
+	"project-app-inventaris-cli-rahmadhany/service"
+)
 
 func main() {
-	fmt.Println("Hello World")
+	// Initialize PostgreSQL database connection
+	conn, err := db.ConnectDB()
+	if err != nil {
+		log.Fatalf("Database error: %v", err)
+	}
+
+	// Dependency injection for kategori modul
+	kategoriRepo := repository.NewKategoriRepository(conn)
+	kategoriService := service.NewKategoriService(kategoriRepo)
+	kategoriHandler := handler.NewKategoriHandler(kategoriService)
+
+	// Dependency injection for barang ategori modul
+	barangRepo := repository.NewBarangRepository(conn)
+	barangService := service.NewBarangService(barangRepo)
+	barangHandler := handler.NewBarangHandler(barangService)
+
+	// Register Handler
+	cmd.RegisterHandlers(kategoriHandler, barangHandler)
+
+	// Execute CLI
+	if err := cmd.Execute(); err != nil {
+		log.Fatalf("Command execution error: %v", err)
+	}
 }
